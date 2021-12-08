0ca824dde469499e9f9efdf7d39ce160628034d3
Connor Hughes <seanandhughes@gmail.com>
2021-12-08 14:06:51 -0500

    Interviewlist displays interviewers and selected Interviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import 'components/InterviewerListItem.scss';
-import classNames from 'classnames';
+import 'components/InterviewerList.scss';
+import InterviewerListItem from 'components/InterviewerListItem'
 
-export default function InterviewerListItem(props) {
+export default function InterviewerList(props) {
+  const { interviewers } = props;
+  const interviewer = interviewers.map((interviewer, index) => {
+    if (props.interviewer === interviewer.id) {
+      return <InterviewerListItem selected={true} key={interviewer.id} {...interviewer}/>
+    }
+    return <InterviewerListItem key={interviewer.id} {...interviewer}/>
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewer}</ul>
     </section>
   );
 }
